1973e1293a8af8914b66af1ed3993ef06e204470
Leo Carrati <carrati3@gmail.com>
2022-09-20 21:06:24 -0300

    Terminar UI
    Finalizar UI e manipulação do DOM.
    Determinar GameOver e Restart.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let pontosJarvas = 0
 let escolhido = ''
 
 function opcaoAleatoria() {
-    let index = Math.floor(Math.random() * OPCOES.length)
+    let index = Math.floor(Math.random() * (4 - 1) + 1);
     return OPCOES[index]
 }
 
@@ -32,45 +32,110 @@ function jogarPartida(opcaoUsuario) {
     if (opcaoUsuario == 'pedra'){   
         if (opcaoComputador == 'pedra') {
             // return pontosUsuario,
-            resultado = 'Empate';
+            resultado = 'Deu EMPATE';
+            document.querySelector(".logBatalha").innerHTML = 
+            `${jogadorPedra}
+             ${jarvasPedra}
+             <p class="miniResultado">
+                ${resultado}
+             </p>
+             `
         } 
         else if (opcaoComputador == 'papel') {
             pontosJarvas++;
+            
             // return pontosJarvas,pontosUsuario,
-            resultado = 'Você perdeu!';
+            resultado = '+1 para Jarvas!';
+            document.querySelector(".logBatalha").innerHTML = 
+            `${jogadorPedra}
+             ${jarvasPapel}
+             <p class="miniResultado">
+                ${resultado}
+             </p>
+             `
         }
         else {pontosUsuario++; 
             // return pontosJarvas,pontosUsuario, 
-            resultado = 'Você venceu!';}
+            resultado = '+1 para você!';
+            document.querySelector(".logBatalha").innerHTML = 
+            `${jogadorPedra}
+             ${jarvasTesoura}
+             <p class="miniResultado">
+                ${resultado}
+             </p>
+             `}
     }
     else if (opcaoUsuario == 'papel'){   
         if (opcaoComputador == 'pedra') {
             pontosUsuario++;
             // return pontosJarvas, pontosUsuario, 
-            resultado = 'Você venceu!';
-        } 
+            resultado = '+1 para você!';
+            document.querySelector(".logBatalha").innerHTML = 
+            `${jogadorPapel}
+             ${jarvasPedra}
+             <p class="miniResultado">
+                ${resultado}
+             </p>
+             `}
+        
         else if (opcaoComputador == 'papel') {
             // return pontosJarvas,pontosUsuario,
-            resultado = 'Empate';
+            resultado = 'Deu EMPATE';
+            document.querySelector(".logBatalha").innerHTML = 
+            `${jogadorPapel}
+             ${jarvasPapel}
+             <p class="miniResultado">
+                ${resultado}
+             </p>
+             `           
         }
         else {pontosJarvas++; 
             // return pontosJarvas,pontosUsuario,
-            resultado = 'Você perdeu!';}
-    }
+            resultado = '+1 para Jarvas!';
+            document.querySelector(".logBatalha").innerHTML = 
+            `${jogadorPapel}
+             ${jarvasTesoura}
+             <p class="miniResultado">
+                ${resultado}
+             </p>
+             `   }
+        }
     else if (opcaoUsuario == 'tesoura'){   
         if (opcaoComputador == 'pedra') {
             pontosJarvas++;
             // return pontosJarvas,pontosUsuario, 
-            resultado = 'Você perdeu!';
+            resultado = '+1 para Jarvas!'
+            ;
+            document.querySelector(".logBatalha").innerHTML = 
+            `${jogadorTesoura}
+             ${jarvasPedra}
+             <p class="miniResultado">
+                ${resultado}
+             </p>
+             `
         } 
         else if (opcaoComputador == 'papel') {
             pontosUsuario++;
             // return pontosJarvas,pontosUsuario, 
-            resultado = 'Você venceu!';
+            resultado = '+1 para você!';
+            document.querySelector(".logBatalha").innerHTML = 
+            `${jogadorTesoura}
+             ${jarvasPapel}
+             <p class="miniResultado">
+                ${resultado}
+             </p>
+             `
         }
         else {
             // return pontosJarvas,pontosUsuario, 
-            resultado = 'Empate';}
+            resultado = 'Deu EMPATE';
+            document.querySelector(".logBatalha").innerHTML = 
+            `${jogadorTesoura}
+             ${jarvasTesoura}
+             <p class="miniResultado">
+                ${resultado}
+             </p>
+             `}
       
     }
     // else {console.log('error');}
@@ -81,8 +146,69 @@ function jogarPartida(opcaoUsuario) {
     if (pontosUsuario >= 3 || pontosJarvas >= 3){
         botoesOpcao.forEach(function (item) {
             item.disabled = true;})
-        console.log('Mostrar popup informando resultado e um botao para reiniciar a partida. Se sim zera os pontos e reabilita os botoes. Senão deixa a mensagem na tela');}
-}    
+        if (pontosUsuario >= 3) {
+            document.querySelector(".logBatalha").innerHTML = `
+            <p class="resultadoFinal">Parabéns! Você venceu!</p>
+            <button class="restart" onclick="restart()">Jogar novamente</button>`
+        }
+        if (pontosJarvas >= 3){
+            document.querySelector(".logBatalha").innerHTML = `
+            <p class="resultadoFinal">Que pena! Você perdeu!</p>
+            <button class="restart" onclick="restart()">Jogar novamente</button>`
+
+
+
+            console.log('Mostrar popup informando resultado e um botao para reiniciar a partida. Se sim zera os pontos e reabilita os botoes. Senão deixa a mensagem na tela');}
+    }    
+}
+
+function restart() {
+    botoesOpcao.forEach(function (item) {
+        item.disabled = false;})
+    pontosJarvas = 0;
+    pontosUsuario = 0;
+    document.querySelector(".logBatalha").innerHTML = `
+    <p>Log de batalha</p>`
+    document.querySelector(".pontosUser > .numeroPontos").innerHTML = `<p>${pontosUsuario}</p>`
+    document.querySelector(".pontosJarvas > .numeroPontos").innerHTML = `<p>${pontosJarvas}</p>` 
+}
+
+
+const jogadorPedra = `
+<p>
+    Você escolheu <strong>&nbsp;PEDRA&nbsp;</strong>
+    <img src="./imagens/sketch-logo.svg" alt="pedra">
+</p>`
+const jogadorPapel = `
+<p>
+    Você escolheu <strong>&nbsp;PAPEL&nbsp;</strong>
+    <img src="./imagens/newspaper-clipping.svg" alt="papel">
+</p>`
+const jogadorTesoura = `
+<p>
+    Você escolheu <strong>&nbsp;TESOURA&nbsp;</strong>
+    <img src="./imagens/scissors.svg" alt="tesoura">
+</p>`
+
+const jarvasPedra = `
+<p>
+    Jarvas escolheu <strong>&nbsp;PEDRA&nbsp;</strong>
+    <img src="./imagens/sketch-logo.svg" alt="pedra">
+</p>`
+
+const jarvasPapel = `
+<p>
+    Jarvas escolheu <strong>&nbsp;PAPEL&nbsp;</strong>
+    <img src="./imagens/newspaper-clipping.svg" alt="papel">
+</p>`
+const jarvasTesoura = `
+<p>
+    Jarvas escolheu <strong>&nbsp;TESOURA&nbsp;</strong>
+    <img src="./imagens/scissors.svg" alt="tesoura">
+</p>`
+
+
+
 
 
 
